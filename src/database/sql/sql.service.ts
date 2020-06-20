f52962d9b3436cd454c28b570d1dbc7970639b3c
Renan Zulian <renanzulian@gmail.com>
2020-06-20 16:31:49 -0300

    Including entitys path

--- src/database/sql/sql.service.ts
+++ src/database/sql/sql.service.ts
@@ -6,17 +6,28 @@ import 'dotenv';
 @Injectable()
 export class SqlService {
   public static getConnection(): TypeOrmModuleOptions {
+    const defaultConfig: TypeOrmModuleOptions = {
+      synchronize: false,
+      entities: ['src/app/**/models/*.ts', 'src/**/*.entity.ts'],
+      migrations: ['src/database/migrations/*.ts'],
+    };
     switch (String(process.env.NODE_ENV)) {
       case 'test':
-        return { type: 'sqlite', database: 'data/test.db', logging: true };
+        return {
+          ...defaultConfig,
+          type: 'sqlite',
+          database: 'data/test.db',
+          logging: true,
+        };
         break;
       default:
         return {
+          ...defaultConfig,
           type: 'postgres',
           host: process.env.SQL_HOST,
           port: Number(process.env.SQL_PORT),
           username: process.env.SQL_USER,
-          password: process.env.SQL_PASS,
+          password: String(process.env.SQL_PASS),
           database: process.env.SQL_NAME,
           logging: true,
         };
